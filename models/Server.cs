8b773be71d0bfeaa7ddf50708cba772c6455c928
Fanera <egor.powwer@gmail.com>
2025-11-13 18:40:39 +1000

    server model

--- models/Server.cs
+++ models/Server.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,117 @@ namespace MiniMessenger.models
     public class Server
     {
         private TcpListener listener;
-        
+        private List<ChatManager> chatManagers = new List<ChatManager>();
+        private bool isRunning = false;
+
+        public event Action<Message> MessageReceived;
+        public event Action<List<string>> UserListUpdated;
+
+        public async Task StartServer(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            isRunning = true;
+
+            _ = Task.Run(async () =>
+
+            {
+                while (isRunning)
+                {
+                    try
+                    {
+                        var client = await listener.AcceptTcpClientAsync();
+                        var handler = new ChatManager(client);
+                        handler.MessageReceived += OnMessageReceived;
+                        handler.ClientDisconnected += OnClientDisconnected;
+                        _ = handler.HandleClientAsync();
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        break;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error accepting client: {ex.Message}");
+                    }
+                }
+            });
+        }
+        public void StopServer()
+        {
+            isRunning = false;
+            listener?.Stop();
+            foreach (var client in chatManagers.ToArray())
+            {
+                client.Disconnect();
+            }
+            chatManagers.Clear();
+        }
+
+        private void OnMessageReceived(Message message, ChatManager sender)
+        {
+            MessageReceived?.Invoke(message);
+
+            switch (message.MessageType)
+            {
+                case TypeMessage.Text:
+                    BroadcastMessage(message, sender);
+                    break;
+                case TypeMessage.Connection:
+                    UpdateUserList();
+                    BroadcastMessage(new Message
+                    {
+                        Author = "Server",
+                        Text = $"{message.Author} joined the chat",
+                        MessageType = TypeMessage.Text,
+                        CreateTime = DateTime.Now
+                    });
+                    break;
+            }
+        }
+
+        private void OnClientDisconnected(ChatManager client,string username)
+        {
+            chatManagers.Remove(client);
+            UpdateUserList();
+            BroadcastMessage(new Message
+            {
+                Author = "Server",
+                Text = $"{username} left the chat",
+                CreateTime = DateTime.Now,
+                MessageType = TypeMessage.Text
+            });
+        }
+        private void BroadcastMessage(Message message, ChatManager excludeSender = null)
+        {
+            foreach (var client in chatManagers)
+            {
+                if (client != excludeSender) _ = client.SendMessageAsync(message);
+            }
+        }
+
+        private void UpdateUserList()
+        {
+            var users = new List<string>();
+            foreach (var user in chatManagers)
+            {
+                if (!string.IsNullOrEmpty(user.Username))
+                {
+                    users.Add(user.Username);
+                }
+            }
+            UserListUpdated?.Invoke(users);
+
+            var userListMessage = new Message
+            {
+                MessageType = TypeMessage.UserList,
+                Text = string.Join(", ", users),
+                CreateTime = DateTime.Now,
+            };
+            foreach (var user in chatManagers)
+            {
+                _ = user.SendMessageAsync(userListMessage);
+            }
+        }
     }
 }
